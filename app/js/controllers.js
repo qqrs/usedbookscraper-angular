0fd1a592e5ceec6809727946ae2f87c6197904bf
Russell Smith <qqrsmith@gmail.com>
2013-08-10 19:27:38 -0400

    Test jsonp call to xisbn api using $resource

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,14 +4,38 @@
 
 (function() {
 
-  function MyCtrl1($scope, $http) {
-    var url = "http://xisbn.worldcat.org/webservices/xid/isbn/9780465067107?method=getEditions&fl=form,lang,author,ed,year,isbn,title&format=json&callback=JSON_CALLBACK";
+  function MyCtrl1($scope, $http, $resource) {
+    /*
     $http.jsonp(url)
       .success(function(data, status) {
           console.log(data);
       }).error(function(data, status) {
           console.log('Error: ' + status);
       });
+    */
+
+    //var url = "http://xisbn.worldcat.org/webservices/xid/isbn/9780465067107?method=getEditions&fl=form,lang,author,ed,year,isbn,title&format=json&callback=JSON_CALLBACK";
+    var xisbn_url = "http://xisbn.worldcat.org/webservices/xid/isbn/:isbn";
+    var xisbn_params = {
+      method: 'getEditions',
+      fl: 'form,lang,author,ed,year,isbn,title',
+      format: 'json',
+      callback: 'JSON_CALLBACK',
+    };
+
+    var xisbn = $resource( xisbn_url, {}, {
+          get_editions: {
+            method: 'JSONP',
+            params: xisbn_params
+          }
+        });
+    //var obj = xisbn.get_editions();
+    //console.log(obj.list);
+    xisbn.get_editions(
+      {isbn: '9780465067107'},
+      function(data, status) { console.log(data); },
+      function(data, status) { console.log('Error: ' + status); }
+    );
 
   }
 
@@ -21,13 +45,14 @@
 
   MyCtrl1.$inject = [
     '$scope',
-    '$http'
+    '$http',
+    '$resource'
   ];
 
   MyCtrl2.$inject = [
   ];
 
-  angular.module('myApp.controllers', [])
+  angular.module('myApp.controllers', ['ngResource'])
     .controller('MyCtrl1', MyCtrl1)
     .controller('MyCtrl2', MyCtrl2);
 })();
