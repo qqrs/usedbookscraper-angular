c8d09916540f732ade0874dab7e3550cfc7fa0aa
Russell Smith <qqrsmith@gmail.com>
2013-08-10 23:35:53 -0400

    Rename xisbn API service and add XisbnService to hide munging

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,14 +16,9 @@
 
 
     $scope.submitISBN = function submitISBN(isbn) {
-      XisbnService.getEditions(
-        {isbn: isbn},
-        function(data, status) { 
-          $scope.altEditions = mungeXisbnEditions(data.list);
-          console.log(data); 
-        },
-        function(data, status) { console.log('Error: ' + status); }
-      );
+      XisbnService.getEditions(isbn, function (editions) {
+          $scope.altEditions = editions;
+      });
     };
 
     $scope.submitISBNList = function submitISBNList(isbnlist) {
@@ -35,57 +30,16 @@
 
   }
 
-
-  // filter unwanted editions and munge each edition object
-  function mungeXisbnEditions(raw_editions) {
-    var editions = [];
-
-    angular.forEach(raw_editions, function(ed) {
-      // filter to English and book formats (BA=book BB=hardcover BC=paperback)
-      if (ed.lang === 'eng' && ed.form[0] && 
-          (ed.form[0] === 'BA' || ed.form[0] === 'BB' || ed.form[0] === 'BC')) {
-        this.push({
-          'isbn':     ed.isbn[0],
-          'title':    ed.title,
-          'author':   ed.author,
-          'lang':     ed.lang,
-          'ed':       ed.ed,
-          'year':     ed.year
-        });
-      }
-    }, editions);
-
-    console.log(editions);
-    return editions;
-  }
-
-  /*
-    alt_editions = xisbn_get_editions(book.isbn)
-      .select {|e| e.lang == "eng" && 
-        e.form && e.form.any? {|f| %w'BA BB BC'.include?(f) } }
-      .sort_by{|e| e.year || "9999" }.reverse
-
-    alt_editions.each do |alt_ed|
-      edition = book.editions.where(isbn: alt_ed.isbn.first).first_or_create(
-          isbn:     (alt_ed.isbn.first || "")[0,255],
-          title:    (alt_ed.title || "")[0,255],
-          author:   (alt_ed.author || "")[0,255],
-          language: (alt_ed.lang || "")[0,255],
-          ed:       (alt_ed.ed || "")[0,255],
-          published_date:   (alt_ed.year || "")[0,255]
-      )
-    end
-  */
-
-
-  function MyCtrl2() {
-
-  }
-
   MyCtrl1.$inject = [
     '$scope',
     'XisbnService'
   ];
+
+// =============================================================================
+
+  function MyCtrl2() {
+
+  }
 
   MyCtrl2.$inject = [
   ];
--- app/js/services.js
+++ app/js/services.js
@@ -4,8 +4,8 @@
 
 (function() {
 
-  function XisbnService($resource) {
-    var xisbn = $resource(
+  function XisbnAPI($resource) {
+    return $resource(
       "http://xisbn.worldcat.org/webservices/xid/isbn/:isbn",
       {
         format: 'json',
@@ -20,16 +20,63 @@
           }
       }
     });
-
-    return xisbn;
   }
 
-  XisbnService.$inject = [
+  XisbnAPI.$inject = [
     '$resource'
   ];
 
+// =============================================================================
+
+
+  function XisbnService(XisbnAPI) {
+    return {
+      'getEditions': function(isbn, successCallback) {
+        XisbnAPI.getEditions(
+          {isbn: isbn},
+          function(data) { 
+            console.log(data); 
+            successCallback(mungeXisbnEditions(data.list));
+          },
+          // TODO: failure callback
+          function(data, status) { console.log('Error: ' + status); }
+        );
+      }
+    };
+  }
+
+  XisbnService.$inject = [
+    'XisbnAPI'
+  ];
+
+  // filter unwanted editions and munge each edition object
+  function mungeXisbnEditions(raw_editions) {
+    var editions = [];
+
+    angular.forEach(raw_editions, function(ed) {
+      // filter to English and book formats (BA=book BB=hardcover BC=paperback)
+      if (ed.lang === 'eng' && ed.form[0] && 
+          (ed.form[0] === 'BA' || ed.form[0] === 'BB' || ed.form[0] === 'BC')) {
+        this.push({
+          'isbn':     ed.isbn[0],
+          'title':    ed.title,
+          'author':   ed.author,
+          'lang':     ed.lang,
+          'ed':       ed.ed,
+          'year':     ed.year
+        });
+      }
+    }, editions);
+
+    console.log(editions);
+    return editions;
+  }
+
+// =============================================================================
+
   angular.module('myApp.services', ['ngResource'])
     .value('version', '0.1')
+    .factory('XisbnAPI', XisbnAPI)
     .factory('XisbnService', XisbnService);
 
 })();
