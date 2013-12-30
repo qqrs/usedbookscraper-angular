5c5bb18406dd8433527c4d302dd192c2f979e54c
Russell Smith <russell@octopart.com>
2013-12-30 03:41:44 -0500

    Only search listings for selected editions

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -43,6 +43,8 @@
     };
     $scope.finishLoading = function () {
       $scope.loading = false;
+      // TODO: testing: select last shelf
+      $scope.submitGoodreadsShelves([$scope.shelves[$scope.shelves.length - 1]]);
     };
   }
 
@@ -56,7 +58,7 @@
 
 // =============================================================================
 
-  function BooksCtrl($scope, $location, BookScraperMaster, GoodreadsService) {
+  function BooksCtrl($scope, $location, $timeout, BookScraperMaster, GoodreadsService) {
     var books = [];
 
     console.log(BookScraperMaster);
@@ -82,7 +84,7 @@
     }, true);
 
     // TODO: sort results by author?
-    // TODO: per-book filter settings: max price, condition, exclude library, desirability weight
+    // TODO: per-book filter settings: max price, condition, exclude library, desirability weight (must-have, normal, add-on only)
 
     //books = [{"id":2659696,"isbn":"0198570503","title":"Global Catastrophic Risks","author":"Nick Bostrom","image_url":"http://www.goodreads.com/assets/nocover/111x148.png","small_image_url":"http://www.goodreads.com/assets/nocover/60x80.png","link":"http://www.goodreads.com/book/show/2659696-global-catastrophic-risks"},{"id":12311305,"isbn":"1741797152","title":"Lonely Planet Vietnam","author":"Iain Stewart","image_url":"http://www.goodreads.com/assets/nocover/111x148.png","small_image_url":"http://www.goodreads.com/assets/nocover/60x80.png","link":"http://www.goodreads.com/book/show/12311305-lonely-planet-vietnam"},{"id":3990666,"isbn":"0864426704","title":"Lonely Planet Cambodia","author":"Nick Ray","image_url":"http://www.goodreads.com/assets/nocover/111x148.png","small_image_url":"http://www.goodreads.com/assets/nocover/60x80.png","link":"http://www.goodreads.com/book/show/3990666-lonely-planet-cambodia"},{"id":552702,"isbn":"1562790692","title":"Pharmako/Poeia: Plant Powers, Poisons, and Herbcraft","author":"Dale Pendell","image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1330212138m/552702.jpg","small_image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1330212138s/552702.jpg","link":"http://www.goodreads.com/book/show/552702.Pharmako_Poeia"},{"id":828377,"isbn":"1843303299","title":"Men and Sheds","author":"Gordon Thorburn","image_url":"http://www.goodreads.com/assets/nocover/111x148.png","small_image_url":"http://www.goodreads.com/assets/nocover/60x80.png","link":"http://www.goodreads.com/book/show/828377.Men_and_Sheds"},{"id":27333,"isbn":"0618249060","title":"Silent Spring","author":"Rachel Carson","image_url":"http://www.goodreads.com/assets/nocover/111x148.png","small_image_url":"http://www.goodreads.com/assets/nocover/60x80.png","link":"http://www.goodreads.com/book/show/27333.Silent_Spring"},{"id":249049,"isbn":"0312361653","title":"Men's Style: The Thinking Man's Guide to Dress","author":"Russell Smith","image_url":"http://www.goodreads.com/assets/nocover/111x148.png","small_image_url":"http://www.goodreads.com/assets/nocover/60x80.png","link":"http://www.goodreads.com/book/show/249049.Men_s_Style"},{"id":12952273,"isbn":null,"title":"Race Against The Machine: How the Digital Revolution is Accelerating Innovation, Driving Productivity, and Irreversibly Transforming Employment and the Economy","author":"Erik Brynjolfsson","image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1329373179m/12952273.jpg","small_image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1329373179s/12952273.jpg","link":"http://www.goodreads.com/book/show/12952273-race-against-the-machine"},{"id":639076,"isbn":"0393326551","title":"Opening Skinner's Box: Great Psychological Experiments of the Twentieth Century","author":"Lauren Slater","image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1361744788m/639076.jpg","small_image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1361744788s/639076.jpg","link":"http://www.goodreads.com/book/show/639076.Opening_Skinner_s_Box"},{"id":254497,"isbn":"0192805851","title":"Consciousness: A Very Short Introduction","author":"Susan J. Blackmore","image_url":"http://www.goodreads.com/assets/nocover/111x148.png","small_image_url":"http://www.goodreads.com/assets/nocover/60x80.png","link":"http://www.goodreads.com/book/show/254497.Consciousness"},{"id":415,"isbn":"0143039946","title":"Gravity's Rainbow","author":"Thomas Pynchon","image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1327868134m/415.jpg","small_image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1327868134s/415.jpg","link":"http://www.goodreads.com/book/show/415.Gravity_s_Rainbow"},{"id":6759,"isbn":"0316921173","title":"Infinite Jest","author":"David Foster Wallace","image_url":"http://www.goodreads.com/assets/nocover/111x148.png","small_image_url":"http://www.goodreads.com/assets/nocover/60x80.png","link":"http://www.goodreads.com/book/show/6759.Infinite_Jest"},{"id":14185,"isbn":null,"title":"The Three Stigmata of Palmer Eldritch","author":"Philip K. Dick","image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1338461946m/14185.jpg","small_image_url":"http://d202m5krfqbpi5.cloudfront.net/books/1338461946s/14185.jpg","link":"http://www.goodreads.com/book/show/14185.The_Three_Stigmata_of_Palmer_Eldritch"}];
     BookScraperMaster.books = books;
@@ -118,12 +120,15 @@
     $scope.finishLoading = function () {
       $scope.setAllSelections(true);
       $scope.loading = false;
+      // TODO: testing: continue with all books selected
+      $timeout(function () {$scope.submitSelectedBooks($scope.selected_books);});
     };
   }
 
   BooksCtrl.$inject = [
     '$scope',
     '$location',
+    '$timeout',
     'BookScraperMaster',
     'GoodreadsService'
   ];
@@ -144,7 +149,7 @@
     $scope.remaining_requests = 0;
 
     // get alternate editions for each book
-    angular.forEach(books, function(book) {
+    angular.forEach(books, function (book) {
       $scope.remaining_requests++;
       XisbnService.getEditions(book.isbn, function (book_editions) {
         book.editions = book_editions
@@ -201,7 +206,6 @@
       }
     };
 
-
     $scope.submitSelectedEditions = function (selection) {
       BookScraperMaster.edition_selections = selection;
       $location.path('/listings');
@@ -225,6 +229,7 @@
   function ListingsCtrl($scope, BookScraperMaster, HalfService) {
     var books = BookScraperMaster.selected_books;
     var editions = BookScraperMaster.editions;
+    var selection = BookScraperMaster.edition_selections;
     var listings = [];
 
     BookScraperMaster.listings = listings;
@@ -259,9 +264,14 @@
     // TODO: cancel requests if leaving controller
 
     // get Half.com listings for each edition of each book
-    angular.forEach(books, function (book) {
+    angular.forEach(books, function (book, book_index) {
       book['listings'] = [];
-      angular.forEach(book.editions, function (ed) {
+      angular.forEach(book.editions, function (ed, ed_index) {
+        if (!selection[book_index][ed_index]) {
+          console.log('         ed.isbn: ' + ed.isbn + '; book_index: ' + book_index + '; ed_index: ' + ed_index);
+          return;
+        }
+        console.log('selected ed.isbn: ' + ed.isbn + '; book_index: ' + book_index + '; ed_index: ' + ed_index);
         ed['listings'] = [];
         HalfService.findItems(
           {isbn: ed.isbn, page: '1', condition: 'Good', maxprice: 4.00},
