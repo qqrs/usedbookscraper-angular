c610b767933575948bad002aa55f3d01a00ed1ee
Russell Smith <russell@octopart.com>
2014-01-19 07:31:18 -0500

    Remove old routes

--- app/js/app.js
+++ app/js/app.js
@@ -17,10 +17,8 @@ angular.module('myApp', ['ngRoute', 'myApp.filters', 'myApp.services', 'myApp.di
     $routeProvider.when('/editions', {templateUrl: 'partials/editions.html', controller: 'EditionsCtrl'});
     $routeProvider.when('/listings', {templateUrl: 'partials/listings.html', controller: 'ListingsCtrl'});
     $routeProvider.when('/sellers', {templateUrl: 'partials/sellers.html', controller: 'SellersCtrl'});
-    //$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    //$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    //$routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
+
     $routeProvider.when('/test', {templateUrl: 'partials/test.html', controller: 'TestCtrl'});
-    //$routeProvider.otherwise({redirectTo: '/user'});
-    $routeProvider.otherwise({redirectTo: '/test'});
+    $routeProvider.otherwise({redirectTo: '/user'});
+    //$routeProvider.otherwise({redirectTo: '/test'});
   }]);
