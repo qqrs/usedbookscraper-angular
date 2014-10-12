b258739073cee3c6b6830749b463f166139df101
Russell Smith <russell@octopart.com>
2014-10-12 19:50:32 -0400

    Remove about page

--- app/app.js
+++ app/app.js
@@ -24,7 +24,6 @@ angular.module('ubsApp', [
   */
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/home.html'});
-    $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
     $routeProvider.when('/example', {templateUrl: 'partials/example.html'});
 
     $routeProvider.when('/start', {templateUrl: 'bookscrapersession/partials/user.html', controller: 'GoodreadsUserCtrl'});
